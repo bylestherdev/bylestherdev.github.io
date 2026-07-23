6804c5bd4e8d498de8520955ecf9ec070d097d91
Jordi Lesther Dev <jordilestherdev@gmail.com>
2026-07-23 01:53:47 -0400

    Fix: sitemap url invalidas google console

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https:/www.bylesther.dev";
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.bylesther.dev/";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
